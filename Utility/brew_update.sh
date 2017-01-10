9175df77d3eefb736d6a55abf593660fb28a9532
Aaron McTavish <aamctustwo@users.noreply.github.com>
2017-01-10 10:57:43 +0000

    Fix Travis CI Implementation (#1)
    * Fix SwiftPM CI tests
    
    * Fix Xcode CI tests
    
    * Add `.ruby-version` file
    
    * Update Homebrew before SwiftPM tests
    
    * List currently installed homebrew packages
    
    * Install homebrew formulae if not already installed
    
    * Fix brew update shellscript
    
    * Ensure sufficient modern version of ruby is installed
    
    * Clean travis yml
    
    Per code review by @arnau

--- Utility/brew_update.sh
+++ Utility/brew_update.sh
@@ -3,5 +3,17 @@
 set -euo pipefail
 
 brew update
-brew outdated swiftlint || brew upgrade swiftlint
-brew outdated shellcheck || brew upgrade shellcheck
+
+if brew ls --versions swiftlint > /dev/null; then
+  brew outdated swiftlint || brew upgrade swiftlint
+else
+  brew install swiftlint
+fi
+
+if brew ls --versions shellcheck > /dev/null; then
+  brew outdated shellcheck || brew upgrade shellcheck
+else
+  brew install shellcheck
+fi
+
+brew list
