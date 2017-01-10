3d92c271945b3991586c7cf51b9f6d9c634261bf
Aaron McTavish <aamct@ustwo.com>
2017-01-10 07:07:57 +0000

    Install homebrew formulae if not already installed

--- Utility/brew_update.sh
+++ Utility/brew_update.sh
@@ -3,7 +3,17 @@
 set -euo pipefail
 
 brew update
-brew outdated swiftlint || brew upgrade swiftlint
-brew outdated shellcheck || brew upgrade shellcheck
+
+if brew ls --versions swiftlint > /dev/null; then
+  brew outdated swiftlint || brew upgrade swiftlint
+else
+  brew install swiftlint
+end
+
+if brew ls --versions shellcheck > /dev/null; then
+  brew outdated shellcheck || brew upgrade shellcheck
+else
+  brew install shellcheck
+end
 
 brew list
