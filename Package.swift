9c4db4c4df568c6b26333742e797aa794734cd49
Aaron McTavish <aamct@ustwo.com>
2017-01-19 08:19:04 +0000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
                               .Target(name: "TestKit")])
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 9)
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0)
     ]
 )
